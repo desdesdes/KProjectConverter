6bca9b9d3624a3390b8fa5960e543a3f3b44b826
DesDesDes <b.vries@sanba.nl>
2014-08-11 17:59:54 +0200

    Allow build after get

---
+++ src/KProjectConverter/Options.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace KProjectConverter
+{
+    /// <summary>
+    /// Summary description for Options
+    /// </summary>
+    public class Options
+    {
+        public string RootDirectory { get; set; }
+        public bool Convert { get; set; }
+        public bool ShowHelp { get; set; }
+
+        public void LoadFromCommandLineArgs(string[] args)
+        {
+            if(CheckArg("?", args) || CheckArg("h", args))
+            {
+                ShowHelp = true;
+                return;
+            }
+
+            if(args.Length > 1)
+            {
+                throw new Exception("First command line arg must be the path to process.");
+            }
+
+            RootDirectory = args[0];
+
+            Convert = CheckArg("c", args);
+        }
+
+        private static bool CheckArg(string argToCheck, string[] args)
+        {
+            var argOptions = new[] {"/" + argToCheck, "-"+ argToCheck, "--" + argToCheck };
+            if (args.Any(i => argOptions.Contains(i, StringComparer.OrdinalIgnoreCase)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
